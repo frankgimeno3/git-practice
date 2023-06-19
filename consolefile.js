e2580a876158e9191e735166aca530043b264793
frankgimeno3 <76010009+frankgimeno3@users.noreply.github.com>
2023-06-19 15:44:44 +0200

    Update consolefile.js
    Just a test

--- consolefile.js
+++ consolefile.js
@@ -1,12 +1,12 @@
-console.log('Hello world');
+console.log('Hello 2 world');
 
-const thisIs = "this is a const";
+const thisIs = "this is a const 2";
 
 console.log(thisIs);
 
-let arrayExample = [1, 2, 3, 4];
+let arrayExample = [2, 2, 3, 1];
 
-let firstFromArray = arrayExample.[0]
-let secondFromArray = arrayExample.[1]
+let nofirstFromArray = arrayExample.[1]
+let nosecondFromArray = arrayExample.[0]
 
-console.log(thisIs, arrayExample, firstFromArray, secondFromArray);
+console.log(thisIs, arrayExample, nofirstFromArray, nosecondFromArray);
